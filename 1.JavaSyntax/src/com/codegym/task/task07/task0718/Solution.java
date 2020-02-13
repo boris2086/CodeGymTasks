3bc0b7c65466de215c16151bbcca04da261db612
Boris Penchev <penchev2086@gmail.com>
2020-02-13 13:30:11 +0200

    13.02

--- 1.JavaSyntax/src/com/codegym/task/task07/task0718/Solution.java
+++ 1.JavaSyntax/src/com/codegym/task/task07/task0718/Solution.java
@@ -10,26 +10,22 @@ Checking the order
 
 */
 public class Solution {
-    public static void main(String[] args) throws IOException {
-        //write your code here
-        BufferedReader reader=new BufferedReader(new InputStreamReader(System.in));
-        ArrayList<String> list = new ArrayList<>();
+	public static void main(String[] args) throws IOException {
+		//write your code here
+		BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
+		ArrayList<String> list = new ArrayList<>();
 
-        for (int i = 0; i < 10; i++) {
-            list.add(reader.readLine());
-        }
-        for (int i = 0; i <list.size() ; i++) {
-            int current = list.get(i).length();
-            for (int j = i+1; j <list.size() ; j++) {
-                int next = list.get(j).length();
-                if (current > next){
-                    System.out.println(i);
-                    return;
-                }
-            }
-        }
+		for (int i = 0; i < 10; i++) {
+			list.add(reader.readLine());
+		}
+		for(int i=0; i<=list.size()-1; i++){
+			int strSizea=list.get(i).length();
+			int strSizeb=list.get(i+1).length();
+			if(strSizea > strSizeb)
+				System.out.println(i-1);
+		}
 
-
-    }
+	}
 }
+
 
